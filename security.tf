5e1d90ae6e4da016a6920af12bc4f1eaee4cb264
JotaOwen <JotaOwen@Gmail.com>
2022-10-13 09:26:57 -0500

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,54 +1,51 @@
 ###
 # Public Security Group
 ##
- 
-resource "aws_security_group" "public" {
-  name = "testing-public-sg"
-  description = "Public internet access"
-  vpc_id = aws_vpc.vpc.id
- 
-  tags = {
-    Name        = "testing-public-sg"
-    Role        = "public"
-    Project     = "testing"
-    Environment = "testing"
-    ManagedBy   = "terraform"
+
+resource "aws_security_group" "sg" {
+  name        = "allow_ssh_http"
+  description = "Allow ssh http inbound traffic"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-}
- 
-resource "aws_security_group_rule" "public_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
- 
-  security_group_id = aws_security_group.public.id
-}
- 
-resource "aws_security_group_rule" "public_in_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.public.id
-}
- 
-resource "aws_security_group_rule" "public_in_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.public.id
-}
- 
-resource "aws_security_group_rule" "public_in_https" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.public.id
+
+
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http"
+  }
 }
