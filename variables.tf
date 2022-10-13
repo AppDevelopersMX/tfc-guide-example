a6bd9a28b5f648ee1922e8fa31c0173beaf73881
JotaOwen <JotaOwen@Gmail.com>
2022-10-13 09:21:40 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,9 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
 }
 
+
+
+variable "creds" {}
+variable "instance_key" {}
+variable "vpc_cidr" {}
+variable "public_subnet_cidr" {}
